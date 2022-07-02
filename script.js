1084f25281a23f4546723daa991d7d34cfc6be28
kgurchiek <90143063+kgurchiek@users.noreply.github.com>
2022-07-01 20:45:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ Array.prototype.forEach.call(
       const filenameList = Array.prototype.map.call(hiddenInput.files, function(
         file
       ) {
-        return file.name;
+        return file.item();
       });
 
       label.textContent = filenameList.join(", ") || defaultLabelText;
